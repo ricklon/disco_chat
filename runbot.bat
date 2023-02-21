17f5e0d667d9871dbd037848f78b061d7e18a2f1
Rick Anderson <rick.rickanderson@gmail.com>
2023-02-21 17:22:15 -0500

    removed extra files and add python .gitignore

---
+++ runbot.bat
@@ -0,0 +1,12 @@
+rem Set the OPENAI_API_KEY environment variable
+set OPENAI_API_KEY=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
+
+rem Set the DISCORD_BOT_TOKEN environment variable
+set DISCORD_BOT_TOKEN=MTA1MDkwNTIwMDc0NTAwMTAwMQ.GPqzO8.TI-VtQK5fgZTURYCQiV4DXQi-qR7SICgV0FVRU
+
+rem Set the DIALOGPT_AUTH_TOKEN environment variable
+set DIALOGPT_AUTH_TOKEN=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
+
+rem Run the Discord bot program using Python
+rem python -m pdb  botfaq.py
+python  botfaq.py
